0db8f95e69655cc6e8b29f9195be97c2d0ba07db
IVANSOLIS0414 <83365994+IVANSOLIS0414@users.noreply.github.com>
2021-04-29 02:04:33 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -58,15 +58,3 @@ cp -r temp/packages/admin/gotop packages/admin/gotop
 cp -r temp/packages/net/minieap packages/net/minieap
 rm -rf temp
 popd
-
-# Set to local feeds
-pushd customfeeds/packages
-export packages_feed="$(pwd)"
-popd
-pushd customfeeds/luci
-export luci_feed="$(pwd)"
-popd
-sed -i '/src-git packages/d' feeds.conf.default
-echo "src-link packages $packages_feed" >> feeds.conf.default
-sed -i '/src-git luci/d' feeds.conf.default
-echo "src-link luci $luci_feed" >> feeds.conf.default
