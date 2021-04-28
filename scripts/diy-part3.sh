f3412707d59010656109f2994f1fd68696ee83da
IVANSOLIS0414 <83365994+IVANSOLIS0414@users.noreply.github.com>
2021-04-29 02:05:47 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -129,6 +129,3 @@ git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
-
-# Get .zshrc dotfile
-cp ../../../data/zsh/.zshrc .
